981d4379ee17530218d6e988c4503496846e929d
Hardik <hardikverma22@gmail.com>
2017-11-26 15:57:43 +0530

    displaying current address in maps activity

--- app/src/main/java/com/example/android/mapsandlocationdemo/MapsActivity.java
+++ app/src/main/java/com/example/android/mapsandlocationdemo/MapsActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,6 +16,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +25,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -75,6 +82,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.clear();
                 mMap.addMarker(new MarkerOptions().position(sydney).title("MY HOME").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15));
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> listAdresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                    if(listAdresses!=null && listAdresses.size()>0)
+                    {
+                        Log.i("place info",listAdresses.get(0).toString());
+                        String address="";
+
+                        if(listAdresses.get(0).getSubThoroughfare()!=null){
+                            address+=listAdresses.get(0).getSubThoroughfare();
+                        }
+                        if(listAdresses.get(0).getThoroughfare()!=null){
+                            address+=listAdresses.get(0).getThoroughfare();
+                        }
+                        if(listAdresses.get(0).getLocality()!=null){
+                            address+=listAdresses.get(0).getLocality();
+                        }
+                        if(listAdresses.get(0).getPostalCode()!=null){
+                            address+=listAdresses.get(0).getPostalCode();
+                        }
+                        if(listAdresses.get(0).getCountryName()!=null){
+                            address+=listAdresses.get(0).getCountryName();
+                        }
+                        Toast.makeText(MapsActivity.this,address,Toast.LENGTH_SHORT).show();
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
@@ -109,28 +145,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                     long minTime,
                                     float minDistance,
                                     LocationListener listener)
-                    Register for location updates using the named provider, and a pending intent.
-                    See requestLocationUpdates(long, float, Criteria, PendingIntent) for more detail on how to use this method.
-                    Requires the ACCESS_COARSE_LOCATION or ACCESS_FINE_LOCATION permissions.
-                    Parameters
-                    provider
-                    String: the name of the provider with which to register
-                    minTime
-                    long: minimum time interval between location updates, in milliseconds
-                    minDistance
-                    float: minimum distance between location updates, in meters
-                    listener
-                    LocationListener: a LocationListener whose onLocationChanged(Location) method will be called for each location update
+                   Use ctrl+q on method to know more
                    */
                     locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER,0,0,locationListener);
                     Location lastKnownLocation = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
-                    LatLng sydney = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+                    LatLng lastknown = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
                     mMap.clear();
-                    mMap.addMarker(new MarkerOptions().position(sydney).title("MY HOME").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15));
+                    mMap.addMarker(new MarkerOptions().position(lastknown).title("MY HOME").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastknown,15));
+                    Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                    try {
+                        List<Address> listAdresses = geocoder.getFromLocation(lastKnownLocation.getLatitude(),lastKnownLocation.getLongitude(),1);
+                        if(listAdresses!=null && listAdresses.size()>0)
+                        {
+                            Log.i("place info",listAdresses.get(0).toString());
+                            String address="";
+
+                            if(listAdresses.get(0).getSubThoroughfare()!=null){
+                                address+=listAdresses.get(0).getSubThoroughfare();
+                            }
+                            if(listAdresses.get(0).getThoroughfare()!=null){
+                                address+=listAdresses.get(0).getThoroughfare();
+                            }
+                            if(listAdresses.get(0).getLocality()!=null){
+                                address+=listAdresses.get(0).getLocality();
+                            }
+                            if(listAdresses.get(0).getPostalCode()!=null){
+                                address+=listAdresses.get(0).getPostalCode();
+                            }
+                            if(listAdresses.get(0).getCountryName()!=null){
+                                address+=listAdresses.get(0).getCountryName();
+                            }
+                            Toast.makeText(MapsActivity.this,address,Toast.LENGTH_SHORT).show();
+
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
 
             }
+
         // Add a marker in Sydney and move the camera
 
     }
